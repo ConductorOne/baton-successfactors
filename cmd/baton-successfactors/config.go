503177df61414877d22abacd6e3fd101af134580
Alex Stanciu <astanciu@gmail.com>
2025-02-21 13:22:54 -0500

    fix cmd folder

--- cmd/baton-successfactors/config.go
+++ cmd/baton-successfactors/config.go
@@ -1,20 +1,64 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/spf13/viper"
 )
 
 var (
-	// ConfigurationFields defines the external configuration required for the
-	// connector to run. Note: these fields can be marked as optional or
-	// required.
-	ConfigurationFields = []field.SchemaField{}
+	BaseURLField = field.StringField(
+		"base-url",
+		field.WithDescription("The base URL for the SAP SuccessFactors OData API (e.g., https://api17.sapsf.com)."),
+		field.WithRequired(true),
+	)
 
-	// FieldRelationships defines relationships between the fields listed in
-	// ConfigurationFields that can be automatically validated. For example, a
-	// username and password can be required together, or an access token can be
-	// marked as mutually exclusive from the username password pair.
+	ClientIDField = field.StringField(
+		"client-id",
+		field.WithDescription("The API key obtained after registering the OAuth 2.0 client application."),
+		field.WithRequired(true),
+	)
+
+	CompanyIDField = field.StringField(
+		"company-id",
+		field.WithDescription("Your SAP SuccessFactors company ID."),
+		field.WithRequired(true),
+	)
+
+	CertPrivateKeyField = field.StringField(
+		"cert-private-key",
+		field.WithDescription("The X.509 certificate private key used to sign the SAML assertion."),
+		field.WithRequired(true),
+	)
+
+	SamlAssertionURLField = field.StringField(
+		"saml-assertion-url",
+		field.WithDescription("The url to use when requesting the oAuth token"),
+		field.WithDefaultValue(""),
+	)
+
+	IdpIssuerURLField = field.StringField(
+		"idp-issuer-url",
+		field.WithDescription("The Issuer URL value for the IdP used to create the SAML assertion."),
+	)
+
+	IdpSubjectNameIDField = field.StringField(
+		"idp-subject-nameid",
+		field.WithDescription("The Subject Name ID value for the user used to create the SAML assertion."),
+	)
+
+	ConfigurationFields = []field.SchemaField{
+		BaseURLField,
+		ClientIDField,
+		CompanyIDField,
+		CertPrivateKeyField,
+		SamlAssertionURLField,
+		IdpIssuerURLField,
+		IdpSubjectNameIDField,
+	}
+
 	FieldRelationships = []field.SchemaFieldRelationship{}
 )
 
@@ -22,6 +66,22 @@ var (
 // error if it isn't valid. Implementing this function is optional, it only
 // needs to perform extra validations that cannot be encoded with configuration
 // parameters.
-func ValidateConfig(v *viper.Viper) error {
+func ValidateConfig(ctx context.Context, v *viper.Viper) error {
+	if v.GetString(BaseURLField.GetName()) == "" {
+		return fmt.Errorf("base URL is required")
+	}
+
+	if v.GetString(ClientIDField.GetName()) == "" {
+		return fmt.Errorf("client ID is required")
+	}
+
+	if v.GetString(CompanyIDField.GetName()) == "" {
+		return fmt.Errorf("company ID is required")
+	}
+
+	if v.GetString(CertPrivateKeyField.GetName()) == "" {
+		return fmt.Errorf("certificate private key is required")
+	}
+
 	return nil
 }
--- cmd/baton-successfactors/main.go
+++ cmd/baton-successfactors/main.go
@@ -1,3 +1,6 @@
+================
+File: cmd/baton-successfactors/main.go
+================
 package main
 
 import (
@@ -9,9 +12,10 @@ import (
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/types"
+	"github.com/conductorone/baton-successfactors/pkg/connector"
+	"github.com/conductorone/baton-successfactors/pkg/connector/client"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"github.com/spf13/viper"
-	"github.com/conductorone/baton-successfactors/pkg/connector"
 	"go.uber.org/zap"
 )
 
@@ -25,7 +29,8 @@ func main() {
 		"baton-successfactors",
 		getConnector,
 		field.Configuration{
-			Fields: ConfigurationFields,
+			Fields:           ConfigurationFields,
+			FieldRelationships: FieldRelationships,
 		},
 	)
 	if err != nil {
@@ -44,19 +49,36 @@ func main() {
 
 func getConnector(ctx context.Context, v *viper.Viper) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
-	if err := ValidateConfig(v); err != nil {
+
+	if err := ValidateConfig(ctx, v); err != nil {
 		return nil, err
 	}
 
-	cb, err := connector.New(ctx)
+	baseURL := v.GetString(BaseURLField.GetName())
+	clientID := v.GetString(ClientIDField.GetName())
+	companyID := v.GetString(CompanyIDField.GetName())
+	certPrivateKey := v.GetString(CertPrivateKeyField.GetName())
+	samlAssertionURL := v.GetString(SamlAssertionURLField.GetName())
+	idpIssuerURL := v.GetString(IdpIssuerURLField.GetName())
+	idpSubjectNameID := v.GetString(IdpSubjectNameIDField.GetName())
+
+	sfClient, err := client.New(ctx, baseURL, clientID, companyID, certPrivateKey, samlAssertionURL, idpIssuerURL, idpSubjectNameID)
+	if err != nil {
+		l.Error("error creating client", zap.Error(err))
+		return nil, err
+	}
+
+	cb, err := connector.New(ctx, sfClient)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
-	connector, err := connectorbuilder.NewConnector(ctx, cb)
+
+	c, err := connectorbuilder.NewConnector(ctx, cb)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
-	return connector, nil
+
+	return c, nil
 }
